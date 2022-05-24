73e38de6df8b5fa67a770bcf6ca55290c7254374
Cássia ☆ <102737875+cassiaplima@users.noreply.github.com>
2022-05-24 08:09:42 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,107 @@
+var paciente = document.querySelector('#primeiro-paciente'); 
+
+var valopeso = paciente.querySelector('.info-peso')
+var peso = parseInt(valopeso.textContent);
+console.log(peso);
+
+var valoaltura = paciente.querySelector('.info-altura');
+var altura = parseInt(valoaltura.textContent);
+console.log(altura);
+
+var valogordura = paciente.querySelector('.info-gordura')
+var gordura = parseInt(valogordura.textContent)
+console.log(gordura)
+
+var imc = (peso+altura+gordura)/3;
+console.log(imc)
+                                                                                                                                                                                                                                                                     
+var substituir = paciente.querySelector('.info-imc')
+substituir.textContent = imc.toFixed(2)
+
+//
+
+var paciente = document.querySelector('#segundo-paciente'); 
+
+var valopeso = paciente.querySelector('.info-peso')
+var peso = parseInt(valopeso.textContent);
+console.log(peso);
+
+var valoaltura = paciente.querySelector('.info-altura');
+var altura = parseInt(valoaltura.textContent);
+console.log(altura);
+
+var valogordura = paciente.querySelector('.info-gordura')
+var gordura = parseInt(valogordura.textContent)
+console.log(gordura)
+
+var imc = (peso+altura+gordura)/3;
+console.log(imc)
+                                                                                                                                                                                                                                                                     
+var substituir = paciente.querySelector('.info-imc')
+substituir.textContent = imc.toFixed(2)
+
+//
+
+var paciente = document.querySelector('#terceiro-paciente'); 
+
+var valopeso = paciente.querySelector('.info-peso')
+var peso = parseInt(valopeso.textContent);
+console.log(peso);
+
+var valoaltura = paciente.querySelector('.info-altura');
+var altura = parseInt(valoaltura.textContent);
+console.log(altura);
+
+var valogordura = paciente.querySelector('.info-gordura')
+var gordura = parseInt(valogordura.textContent);
+console.log(gordura)
+
+var imc = (peso+altura+gordura)/3;
+console.log(imc)
+                                                                                                                                                                                                                                                                     
+var substituir = paciente.querySelector('.info-imc')
+substituir.textContent = imc.toFixed(2)
+
+//
+
+var paciente = document.querySelector('#quatro-paciente'); 
+
+var valopeso = paciente.querySelector('.info-peso')
+var peso = parseInt(valopeso.textContent);
+console.log(peso);
+
+var valoaltura = paciente.querySelector('.info-altura');
+var altura = parseInt(valoaltura.textContent);
+console.log(altura);
+
+var valogordura = paciente.querySelector('.info-gordura')
+var gordura = parseInt(valogordura.textContent);
+console.log(gordura)
+
+var imc = (peso+altura+gordura)/3;
+console.log(imc)
+                                                                                                                                                                                                                                                                     
+var substituir = paciente.querySelector('.info-imc')
+substituir.textContent = imc.toFixed(2)
+
+//
+
+var paciente = document.querySelector('#sinco-paciente'); 
+
+var valopeso = paciente.querySelector('.info-peso')
+var peso = parseInt(valopeso.textContent);
+console.log(peso);
+
+var valoaltura = paciente.querySelector('.info-altura');
+var altura = parseInt(valoaltura.textContent);
+console.log(altura);
+
+var valogordura = paciente.querySelector('.info-gordura')
+var gordura = parseInt(valogordura.textContent);
+console.log(gordura)
+
+var imc = (peso+altura+gordura)/3;
+console.log(imc)
+                                                                                                                                                                                                                                                                     
+var substituir = paciente.querySelector('.info-imc')
+substituir.textContent = imc.toFixed(2)
